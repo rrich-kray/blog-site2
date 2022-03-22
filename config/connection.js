dcd3b1254f1c7f322c4ab2d6a89fe1b85519eb27
rrich-kray <rrich.kray@gmail.com>
2022-03-22 12:15:47 -0400

    added jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
-const sequelize = process.env.JAWS_DB
-  ? new Sequelize(process.env.JAWS_DB)
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: "localhost",
       dialect: "mysql",
