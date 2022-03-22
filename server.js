897492a8dbd3a447b9bbde4cc36aa8edbc9ad413
rrich-kray <rrich.kray@gmail.com>
2022-03-22 12:25:24 -0400

    adjusted server.js file to render handlebars templates on heroku

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const hbs = exphbs.create({});
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
