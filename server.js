63d9713aa9389bbe77000183817d95e6e37ad7a8
rrich-kray <rrich.kray@gmail.com>
2022-03-27 13:39:16 -0400

    Added BSD license

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+// Copyright (c) 2022, Ryan Kray
+// All rights reserved.
+
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
 const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
